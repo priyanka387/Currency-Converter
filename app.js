4c8254558df51599985d060e7509d047ddd6e887
priyanka387 <priyankapathak222@gmail.com>
2024-01-09 01:45:01 +0530

    currency converter final

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
-// const URL = "https://cat-fact.herokuapp.com/facts";
-// const factPara = document.querySelector("#fact");
-// const btn = document.querySelector("#btn");;
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-//async await
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
-// const getFacts = async () => {
-//     console.log("getting data....")
-//     let response = await fetch(URL);
-//     console.log(response);
-//     let data = await response.json();
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
 
-//     factPara.innerText = data[1].text;
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
 
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()];
 
-// }
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
 
-// btn.addEventListener("click", getFacts);
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
 
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
 
-//promise chain
-
-// function getFacts() {
-//     fetch(URL).then((response) => {
-//         return response.json()
-//     })
-//     .then((data) => {
-//         console.log(data)
-//         factPara.innerText = data[1].text;
-//     })
-// };
-
-// btn.addEventListener("click", getFacts);
-
-
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
